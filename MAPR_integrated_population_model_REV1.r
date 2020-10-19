4876f4b27d3701dcef4f6128b67c6e502e324a37
Steffen <steffen.oppel@gmail.com>
2020-10-19 09:51:37 +0100

    added m-array to surv data preparation
    goodness-of-fit test requires aggregated data simulation, hence not possible in simple state-space formulation -> changed survival model to m-array

--- MAPR_integrated_population_model_REV1.r
+++ MAPR_integrated_population_model_REV1.r
@@ -83,11 +83,37 @@ dim(CH)
 rCH<-CH[!(del==1 & del2==TRUE),]
 dim(rCH)
 
-
-
 # Compute vector with occasion of first capture
 get.first <- function(x) min(which(x==1))
 f <- apply(CH, 1, get.first)
+
+#### Create Function to create a m-array based on capture-histories (CH)
+marray <- function(CH){
+  nind <- dim(CH)[1]
+  n.occasions <- dim(CH)[2]
+  m.array <- matrix(data = 0, ncol = n.occasions+1, nrow = n.occasions)
+  # Calculate the number of released individuals at each time period
+  for (t in 1:n.occasions){
+    m.array[t,1] <- sum(CH[,t])
+  }
+  for (i in 1:nind){
+    pos <- which(CH[i,]!=0)
+    g <- length(pos)
+    for (z in 1:(g-1)){
+      m.array[pos[z],pos[z+1]] <- m.array[pos[z],pos[z+1]] + 1
+    } #z
+  } #i
+  # Calculate the number of individuals that is never recaptured
+  for (t in 1:n.occasions){
+    m.array[t,n.occasions+1] <- m.array[t,1] - sum(m.array[t,2:n.occasions])
+  }
+  out <- m.array[1:(n.occasions-1),2:(n.occasions+1)]
+  return(out)
+}
+
+# Create the m-array from the capture-histories
+marr <- marray(CH)
+rmarr <- marray(rCH)
 
 
 
